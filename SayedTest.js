c8f80d0f8135406cef5e3a6f5e4266fe9790aac9
ranaagouda <ranagouda01@gmail.com>
2023-09-02 11:36:32 +0300

    sayed is testing

---
+++ SayedTest.js
@@ -0,0 +1,7 @@
+function test(name, birthyear, currentyear) {
+  const age = function test(name, birthyear, currentyear) {
+ - birthyear;
+  const r = name + "is" + age;
+  return r;
+}
+console.log(test("alice", 1871, 1964));
